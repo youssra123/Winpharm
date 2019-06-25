c57a31098a0bebfdf89dbbfd6bbfeff41336bbec
bouchra mounir <mounir.bouchra98@gmail.com>
2019-06-25 12:41:46 +0100

    deletestockproduit

--- src/main/webapp/app/entities/stockproduit/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './stockproduit.service';
-export * from './stockproduit-update.component';
-export * from './stockproduit-delete-dialog.component';
-export * from './stockproduit-detail.component';
-export * from './stockproduit.component';
-export * from './stockproduit.route';
